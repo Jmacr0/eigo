d387c4e659d68182d2ac6e949a5e8f9a4f72014d
Jmacr0 <jonmacrohon@gmail.com>
2020-08-29 05:28:00 +1000

    require path

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');

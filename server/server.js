29ca9aa337801c0f79db76fb6c23d7acfb914100
Jmacr0 <jonmacrohon@gmail.com>
2020-10-09 16:37:26 +1100

    fix static fiule pathing for prod

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use(passport.session());
 app.use(routes);
 
 if (NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 	app.get('*', (_req, res) => {
 		res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 	})

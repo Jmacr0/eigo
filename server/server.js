61be3929410cde502ee401201fe0ee5a39cef134
Jmacr0 <jonmacrohon@gmail.com>
2020-10-09 12:03:55 +1100

    fix heroku server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 const db = require('./models');
